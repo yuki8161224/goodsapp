5dd29e3e13b3247c77447dd3265a43d7ae817fcd
関谷祐希 <yuki.sekiya@neuromagic.com>
2025-09-21 15:29:50 +0900

    個数入力

--- resources/views/guestgoods/index.blade.php
+++ resources/views/guestgoods/index.blade.php
@@ -8,15 +8,32 @@
     <div class="goods_bg">
         <ul class="goods_wrapper">
             @foreach($goods as $item)
-            <li class="goods_contents">
+            <li class="goods_contents" x-data="{ quantity: 0 }">
                 <h3>{{ $item->goods_name }}</h3>
                 <figure>
                     <img src="{{ asset('storage/' .$item->image_path) }}" alt="{{ $item->goods_name }}">
                     <figcaption>{{ $item->price }}円</figcaption>
                 </figure>
+                <div class="btn-wrapper">
+                    <div class="num-wrapper">
+                        <button type="button" @click="if (quantity > 0) quantity--" class="rembtn"></button>
+                        <input
+                            type="number"
+                            id="quantity-{{ $item->goods_id }}"
+                            min="0"
+                            x-model.number="quantity"
+                            class="w-20 text-center border rounded-md" />
+                        <button type="button" @click="quantity++" class="addbtn"></button>
+                    </div>
+                    <button
+                        type="button"
+                        @click="quantity++">
+                        カートに追加する
+                    </button>
+                </div>
             </li>
             @endforeach
         </ul>
-
+        <a href="" class="total-submit">注文票を表示する</a>
     </div>
 </x-app-layout>
